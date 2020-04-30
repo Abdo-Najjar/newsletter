be33933a322552d7e2eb5c75335e6913a7fb1329
Abdo Najjar <raf7awe.overlord@gmail.com>
2020-04-30 05:42:52 +0300

    set role as client by default

--- app/User.php
+++ app/User.php
@@ -36,4 +36,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    protected $attributes = [
+
+        //set default value for role as aclient
+        'role' => '0',
+    ];
+
 }
